1563d7b88a531dbd4c30b865f02a2c279d4148d1
Jennifer <jennis015@hotmail.com>
2022-05-04 21:09:43 -0300

    entrega

---
+++ Desafios/Desafio-Clase-32/nodeServer/src/utils/options.js
@@ -0,0 +1,24 @@
+const options = {
+  firebase: {
+    "type": "service_account",
+  "project_id": "backend-56667",
+  "private_key_id": "9dbfa9b2139459c882844f196de0a299c8a3c3a4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDPQeBsTfEcMIwL\nD8GJFvnZF3IYG22omNvLMFM3flAOvSjvV+cgukW+jtNLZgSc6DuMSmh1q1644hDO\nTU+cYTnZsB36Bu+nHwnpsWQGxO2OE1MO6nandsUG9VDhhhrxZHPWJV1csPyZX/+F\nmAq9w8ySTM6sxiwEvZep3YdF5FfFgBqmlU8yFdSJDTHEw7lFtOb2y42SqJ9xp/6a\nmxOaG+/+ztI6WAQ308O3/uQ5SD90PE91mJl0aAsGwRK2qIoKRqJmEhvVPCD012zH\nqxjtGoW0ASqcWbfF4zSrt0ejvTOdr81MjQOu0nOOauot6ngyZXoWF0Q5g8rU+jxK\nxtrp7DtzAgMBAAECggEAAeUjY49KThTbwB1Nt/bO/sZZBqRW2jQB7daD+XBLgcfE\nlWxQUqCHEyYHh8KO9kWM4lro6kkazpqT/MsNC+XkhrM+uuyL7MjukFIGzQiXxZoq\n9G51yP5wOvyGcJ9mNXFS33AfmwOjudXPBXWMdH9jtr5KwY4GxocYbe7Zy66QlB2R\nA/BZRmEjz9D+0ayV/lGOYWSMg0HC3Sr79cM/Mwa8EMiy89URrxhA8io2wj7etfvK\niREDHYx2HUiMigLlX5VvGZpeoPazGL5z6C3jkiWa8u8+ybX06grH+F06Au+Yfe0r\nVmakfdWkqZh4s/7YdVrNwJHQlRVp4irloio7uFmDAQKBgQD+wmkdtGRZVM8md6pE\nuGiYcrG6vEbHhcjlm/LHZZCElNhXuDivw0sBmWQPH89gCxeW2AsBPhIRCilpjAua\nAsUKQ6DQozs13tcOdxLC9zPDPY4edxHvK4+xoNlgq6TeYhr58v9RLFb/kHAGJBNN\n1fWiwyze1JoWm5kXZmkEs96ccwKBgQDQRD+vQQgrs0monq5scP3e4qdoNxwxDwP4\nAhcMyUSp9Q6asaEqiGQ4vRJKNnQTLBKeaPOx7Af4IAPlSYfRyfz1MA6l2RuUQ1xV\nHWWVlA0Ol37fcg8scFCEJRVgA0DL+tM+UjzNKGo/vIhchlYrODLJn+3Ig9iDPWhe\nGraQzYslAQKBgQDoJLFyfvneKRjmXa7LFhtBp2nq1jvM/kz1ZajSJ+uL1LJbJKSL\nDPNhTqxN19e5FgJ10wIcLLp7yn24DwRXX+S7GKsG02dWDGx9YuW2wVnqtEG34xp1\njYVQAFl07JO8jTs1vkcHadcQ5EYgYmkwySIfIajWmHac2Uakew6BWYIlnwKBgFSM\nwIX3ksea+7ZW5Vzk6kvIHEp0xR8th2DyCEWKmDOfZxRqNJG92WInb/ts6+a0p8Jj\nvTtoJTwYUzyMg3Up2WX44pl18HpadmufiZQWDM71te4ksgBKgIWFQI5AA9hLteLB\nJCnt1GSXt4xIhUb6g7PTF4el7woorew2UBOtPmYBAoGADd7llRiSy04ev7E00BSK\nIMgD4UjR8ascX38ilePXTG6ohHH94DSX8B2ELUQzOazCWGXaK2opJj4SBv+wE2Iq\nQXNbgbNPIMMJ1/ycHuKph0jFhkBmZZtAjcNxJW53J+HVRp7gEyBnltWAcizhq8MY\nVuuotuHI13m8JGr3k1MF9OI=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-x442q@backend-56667.iam.gserviceaccount.com",
+  "client_id": "113481841135211269123",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-x442q%40backend-56667.iam.gserviceaccount.com"
+  },
+  mongoAtlas: {
+    mongourl:
+      "mongodb+srv://jennifer:coderhouse@cluster0.9cfys.mongodb.net/sesiones?retryWrites=true&w=majority",
+  },
+  facebookId: {
+    facebook_app_id: "683945282935309",
+    facebook_app_secret: "8ace0f322bfe550f8b0e8c9725ea7f48",
+  },
+};
+
+export default options;
