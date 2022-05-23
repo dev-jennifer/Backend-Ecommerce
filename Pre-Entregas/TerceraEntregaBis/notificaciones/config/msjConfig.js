af01b2b1e477de4234900ee2d93f9d6b19c58a0c
Jennifer <jennis015@hotmail.com>
2022-05-22 21:34:15 -0300

    .

--- Pre-Entregas/TerceraEntregaBis/notificaciones/config/msjConfig.js
+++ Pre-Entregas/TerceraEntregaBis/notificaciones/config/msjConfig.js
@@ -11,27 +11,26 @@ const client = twilio(ACCOUNT_SID, AUTH_TOKEN,TWILO_PHONE);
 async function msgSend(number, order){
   console.log("order",order);
 
-// try {
-//   //sms to user
-//     const message = await client.messages.create({
-//       body: `Hemos recibido su pedido  se encuentra en estado PROCESANDO`,
-//       from: TWILO_PHONE,
-//       to: number,
-//     });
+try {
+  //sms to user
+    const message = await client.messages.create({
+      body: `Hemos recibido su pedido  se encuentra en estado PROCESANDO`,
+      from: TWILO_PHONE,
+      to: number,
+    });
  
-// } catch (e) {
-//    console.error('SMS CLIENT', e);
-// }
+} catch (e) {
+   console.error('SMS CLIENT', e);
+}
 try {
   //whatsapp to admin
-  console.warn("WHATSAPP_FROM",WHATSAPP_FROM);
-    console.warn("WHATSAPP_ADMIN",WHATSAPP_ADMIN);
-    
-  const smsAdmin = await client.messages.create({
-    from: `whatsapp:${WHATSAPP_FROM}`,
-    to: `whatsapp:${WHATSAPP_ADMIN}`,
-    body: `Nuevo Pedido nº${order.id} de ${order.name} - ${order.buyerID} `,
-  });
+  const smsAdmin = await client.messages
+    .create({
+      from: 'whatsapp:+14155238886',
+      to: `whatsapp:${WHATSAPP_ADMIN}`,
+      body: `Nuevo Pedido nº${order.id}  - ${order.buyerID} `,
+    })
+    .then((smsAdmin) => console.log(JSON.stringify(smsAdmin)));
  
 } catch (e) {
    console.error('SMS admin', e);
